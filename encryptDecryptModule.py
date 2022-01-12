ec63b9e8f3c132c4f7b2b7c5602c1c730ec49aff
Adithya S K <adithyaskolavi@gmail.com>
2022-01-12 23:22:02 +0530

    Reverse encryption and dectryption done💪 - ASK

---
+++ encryptDecryptModule.py
@@ -0,0 +1,10 @@
+def reverseEncrypt(inputMessage):
+    strInput = str(inputMessage)
+    reversStr = strInput[::-1]
+    return str(reversStr)
+
+def reverseDecrypt(inputMessage):
+    strInput = str(inputMessage)
+    reversStr = strInput[::-1]
+    return str(reversStr)
+
