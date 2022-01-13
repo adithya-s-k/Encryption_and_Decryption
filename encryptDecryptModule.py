b66c9b129ed450f7a72736995dfde87a8728e37c
Adithya S K <adithyaskolavi@gmail.com>
2022-01-13 11:24:56 +0530

    Base 64 encrytption and Decryption done💪 - ASK

--- encryptDecryptModule.py
+++ encryptDecryptModule.py
@@ -1,3 +1,10 @@
+# import other necessery modules
+import random
+# modules for encryption and decryption
+import base64
+import onetimepad
+import pyDes
+
 def reverseEncrypt(inputMessage):
     strInput = str(inputMessage)
     reversStr = strInput[::-1]
@@ -31,3 +38,17 @@ def caeserCipherDecrypt(string, shift):
             cipher = cipher + chr((ord(char) + shift - 97) % 26 + 97)
     return str(cipher)
 
+
+def base64Encryption(msg):
+    sample_string = (msg)
+    sample_string_bytes = sample_string.encode("ascii")
+    base64_bytes = base64.b64encode(sample_string_bytes)
+    base64_string = base64_bytes.decode("ascii")
+    return str(base64_string)
+
+def base64Decryption(decode_entry):
+    base64_string = decode_entry
+    base64_bytes = base64_string.encode("ascii")
+    sample_string_bytes = base64.b64decode(base64_bytes)
+    sample_string = sample_string_bytes.decode("ascii")    
+    return sample_string
