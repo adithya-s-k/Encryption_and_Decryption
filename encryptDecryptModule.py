3747539c814fc9fcb5617670f84b434f905d5115
Adithya S K <adithyaskolavi@gmail.com>
2022-01-13 12:41:34 +0530

    4 layer encryption working💪  -ASK

--- encryptDecryptModule.py
+++ encryptDecryptModule.py
@@ -51,4 +51,24 @@ def base64Decryption(decode_entry):
     base64_bytes = base64_string.encode("ascii")
     sample_string_bytes = base64.b64decode(base64_bytes)
     sample_string = sample_string_bytes.decode("ascii")    
-    return sample_string
+    return str(sample_string)
+
+def pydesEncrypt(msg):
+    data = msg
+    k = pyDes.des("DESCRYPT", pyDes.CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
+    d = k.encrypt(data)
+    return d
+
+def pydesDecrypt(msg2):
+    k = pyDes.des("DESCRYPT", pyDes.CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
+    lmao = k.decrypt(msg2).decode('ASCII')
+    return str((bytes(lmao, 'utf-8')).decode("utf-8"))
+
+
+print(pydesEncrypt
+        (caeserCipherEncrypt
+            (reverseEncrypt("message"),10)
+        ))
+encrypt_message = (pydesEncrypt(base64Encryption(caeserCipherEncrypt(reverseEncrypt("message"),10))))
+
+print(reverseDecrypt(caeserCipherDecrypt(base64Decryption(pydesDecrypt(encrypt_message)),10)))
